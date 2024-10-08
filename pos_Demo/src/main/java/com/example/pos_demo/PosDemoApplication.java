b1e46a5983936eb531d611f5484b6c8ccc012ef8
Akila Lakshitha <136437833+mrakiyaaa@users.noreply.github.com>
2024-10-08 22:30:56 +0530

    Files Updates

---
+++ pos_Demo/src/main/java/com/example/pos_demo/PosDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pos_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosDemoApplication.class, args);
+    }
+
+}
