4b35d02f370e223a28b4d251f20e19e34366eb51
Akila Lakshitha <136437833+mrakiyaaa@users.noreply.github.com>
2024-10-09 00:29:32 +0530

    Update Entity class

--- pos_Demo/src/main/java/com/example/pos_demo/entity/Customer.java
+++ pos_Demo/src/main/java/com/example/pos_demo/entity/Customer.java
@@ -1,4 +1,41 @@
 package com.example.pos_demo.entity;
 
+import com.vladmihalcea.hibernate.type.json.JsonType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.Type;
+
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "customer")
+@TypeDefs({
+        @TypeDef(name = "json",typeClass = JsonType.class)
+})
 public class Customer {
+
+    @Id
+    @Column(name = "customer_id",length = 45)
+    private int customerId;
+
+    @Column(name = "customer_name",length = 45,nullable = false)
+    private String customerName;
+
+    @Column(name = "customer_address",length = 255)
+    private String customerAddres;
+
+    @Type(type = "json")
+    @Column(name = "customer_numbers",columnDefinition = "json")
+    private ArrayList contactNumber;
+
+    @Column(name = "nic")
+    private String nic;
+
+    @Column(name = "active_state",columnDefinition = "TINYINT default 1")
+    private boolean active;
+
+
 }
